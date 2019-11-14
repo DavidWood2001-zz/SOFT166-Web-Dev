55524d6ff263eacb177cb793569eaab93b5e7ceb
DavidWood2001 <davidwood1301@gmail.com>
2019-11-14 17:33:50 +0000

    Added functionality to the play page, can now change light state usin js.

---
+++ js/lightClicked.js
@@ -0,0 +1,46 @@
+function getLightURI(element)
+{
+    var IP = "http://192.168.0.50/api/";
+    var username = "stlaB2I6VZ8O80Qepc-1xfmLrHgyTFvB9IGupaQz";
+    var lights = "/lights/";
+    var URI = IP + username + lights;
+    return URI + element.attr("id")+"/";
+}
+
+function togglelight(element)
+{
+    var getState = $.getJSON(getLightURI(element), function (data)
+    {
+        var state = data["state"]["on"];
+        if (state)
+        {
+            state = false;
+        }
+        else
+        {
+            state = true;
+        }
+
+        var lightState = {"on" : state,
+        "hue":1,
+        "bri":255,
+        "sat":255,
+        "effect":"colorloop",
+        "alert":"lselect"};
+
+        $.ajax({
+            url: getLightURI(element) + "state/",
+            type: "PUT",
+            data: JSON.stringify(lightState)
+        })
+    });
+}
+
+$(document).ready(function()
+{
+    $('td').click(function()
+    {
+        togglelight($(this));
+
+    })
+});
