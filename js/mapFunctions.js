c937af8382f26dab4b8858eb7d41265cbbe78bac
DavidWood2001 <davidwood1301@gmail.com>
2019-11-21 17:35:47 +0000

    Outsourced the navbar, sidebar and all js scripts to individual files.

---
+++ js/mapFunctions.js
@@ -0,0 +1,9 @@
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+
